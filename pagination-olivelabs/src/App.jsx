082603aff7c7d1b1ee320b810930bf164c6495e6
opeyemi0001 <opeyemionanuga40@gmail.com>
2025-04-12 09:34:38 +0100

    patient data table

---
+++ pagination-olivelabs/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <div>
+      <div>
+        <h1 className=" text-3xl font-bold underlineg">Movie List</h1>
+        <input type="text" placeholder="Search by name" />
+      </div>
+    </div>
+  )
+}
+
+export default App
