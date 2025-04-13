7998e6036aa94dc440f6a36f18e03b6a8ca0d3cd
opeyemi0001 <opeyemionanuga40@gmail.com>
2025-04-13 21:57:16 +0100

    feat: pagination

---
+++ pagination-olivelabs/src/assets/data.jsx
@@ -0,0 +1,171 @@
+export const data = [
+  {
+    id: 1,
+    name: "Tolu Tijani",
+    age: 38,
+    gender: "male",
+    diagnosis: "Hypertension",
+    admissionDate: "2024-11-21",
+    action: <div>
+      <button className="viewBtn Btn">View</button>
+      <button className="editBtn Btn">Edit</button>
+    </div>
+  },
+  {
+    id: 2,
+    name: "Titilayo Funmi",
+    age: 20,
+    gender: "male",
+    diagnosis: "Malaria",
+    admissionDate: "2023-09-19",
+    action: <div>
+      <button className="viewBtn Btn ">View</button>
+      <button className="editBtn Btn">Edit</button>
+    </div>
+  },
+  {
+    id: 3,
+    name: "Tunji Tofunmi",
+    age: 19,
+    gender: "Female",
+    diagnosis: "Tuberculosis",
+    admissionDate: "2023-08-23",
+    action: <div>
+      <button className="viewBtn Btn">View</button>
+      <button className="editBtn Btn">Edit</button>
+    </div>
+  },
+  {
+    id: 4,
+    name: "Oluwabayo Oluwaseun",
+    age: 54,
+    gender: "male",
+    diagnosis: "Hypertension",
+    admissionDate: "2022-10-30",
+    action: <div>
+      <button className="viewBtn Btn">View</button>
+      <button className="editBtn Btn">Edit</button>
+    </div>
+  },
+  {
+    id: 5,
+    name: "John Doe",
+    age: 42,
+    gender: "male",
+    diagnosis: "Hypertension",
+    admissionDate: "2024-10-11",
+    action: <div>
+      <button className="viewBtn Btn  ">View</button>
+      <button className="editBtn Btn  ">Edit</button>
+    </div>
+  },
+  {
+    id: 6,
+    name: "williams James",
+    age: 36,
+    gender: "male",
+    diagnosis: "Hypertension",
+    admissionDate: "2023-10-01",
+    action: <div>
+      <button className="viewBtn Btn ">View</button>
+      <button className="editBtn Btn">Edit</button>
+    </div>
+  },
+  {
+    id: 7,
+    name: "Daniel Smith",
+    age: 32,
+    gender: "male",
+    diagnosis: "Hypertension",
+    admissionDate: "2023-10-01",
+    action: <div>
+      <button className="viewBtn Btn ">View</button>
+      <button className="editBtn Btn ">Edit</button>
+    </div>
+  },
+  {
+    id: 8,
+    name: "Titilayo Funmi",
+    age: 32,
+    gender: "male",
+    diagnosis: "Hypertension",
+    admissionDate: "2024-10-11",
+    action: <div>
+      <button className="viewBtn Btn">View</button>
+      <button className="editBtn Btn">Edit</button>
+    </div>
+  },
+  {
+    id: 9,
+    name: "Titilayo Funmi",
+    age: 22,
+    gender: "male",
+    diagnosis: "Hypertension",
+    admissionDate: "2023-10-01",
+    action: <div>
+      <button className="viewBtn Btn">View</button>
+      <button className="editBtn Btn ">Edit</button>
+    </div>
+  },
+  {
+    id: 10,
+    name: "Titilayo Funmi",
+    age: 61,
+    gender: "male",
+    diagnosis: "Hypertension",
+    admissionDate: "2023-08-01",
+    action: <div>
+      <button className="viewBtn Btn">View</button>
+      <button className="editBtn Btn">Edit</button>
+    </div>
+  },
+  {
+    id: 11,
+    name: "Titilayo Funmi",
+    age: 32,
+    gender: "male",
+    diagnosis: "Lassa Fever",
+    admissionDate: "2024-10-01",
+    action: <div>
+      <button className="viewBtn Btn ">View</button>
+      <button className="editBtn Btn">Edit</button>
+    </div>
+  },
+  {
+    id: 12,
+    name: "Titilayo Funmi",
+    age: 52,
+    gender: "male",
+    diagnosis: "covid19",
+    admissionDate: "2023-07-11",
+    action: <div>
+      <button className="viewBtn Btn">View</button>
+      <button className="editBtn Btn">Edit</button>
+    </div>
+  },
+  {
+    id: 13,
+    name: "Titilayo Funmi",
+    age: 32,
+    gender: "male",
+    diagnosis: "Typhoid",
+    admissionDate: "2022-10-01",
+    action: <div>
+      <button className="viewBtn Btn">View</button>
+      <button className="editBtn Btn">Edit</button>
+    </div>
+  },
+  {
+    id: 14,
+    name: "Titilayo Funmi",
+    age: 66,
+    gender: "male",
+    diagnosis: "Hypertension",
+    admissionDate: "2024-03-14",
+    action: <div>
+      <button className="viewBtn Btn">View</button>
+      <button className="editBtn Btn">Edit</button>
+    </div>
+  },
+
+]
